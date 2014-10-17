0cb03b3760311721f37a1145606fbaa0b30efca6
Jake LeBoeuf <dev@jakeleboeuf.com>
2014-10-17 14:08:28 -0400

    testint and examples

---
+++ test/indiegogo.js
@@ -0,0 +1,34 @@
+var expect = require("chai").expect;
+var request = require('supertest');
+var cheerio = require('cheerio');
+var express = require('express');
+var projects = require("../routes/indiegogo.js");
+var app = require("../app");
+
+describe('GET /indiegogo/global-learning-xprize', function(){
+    it('should respond with json', function(done){
+        request(app)
+            .get('/indiegogo/global-learning-xprize')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    })
+    it('should have a project object', function(done){
+        request(app)
+            .get('/indiegogo/global-learning-xprize')
+            .set('Accept', 'application/json')
+            .end(function (err, res) {
+                expect(res.body).to.have.a.property("project")
+                done();
+            });
+    })
+    it('should have a perks object', function(done){
+        request(app)
+            .get('/indiegogo/global-learning-xprize')
+            .set('Accept', 'application/json')
+            .end(function (err, res) {
+                expect(res.body).to.have.a.property("perks")
+                done();
+            });
+    })
+})
--- test/kickstarter.js
+++ test/kickstarter.js
@@ -2,20 +2,20 @@ var expect = require("chai").expect;
 var request = require('supertest');
 var cheerio = require('cheerio');
 var express = require('express');
-var projects = require("../routes/projects.js");
+var projects = require("../routes/kickstarter.js");
 var app = require("../app");
 
-describe('GET /global-learning-xprize', function(){
+describe('GET /kickstarter/332322466/gram-parsons-cosmic-revival-derry-down', function(){
     it('should respond with json', function(done){
         request(app)
-            .get('/global-learning-xprize')
+            .get('/kickstarter/332322466/gram-parsons-cosmic-revival-derry-down')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200, done);
     })
     it('should have a project object', function(done){
         request(app)
-            .get('/global-learning-xprize')
+            .get('/kickstarter/332322466/gram-parsons-cosmic-revival-derry-down')
             .set('Accept', 'application/json')
             .end(function (err, res) {
                 expect(res.body).to.have.a.property("project")
@@ -24,7 +24,7 @@ describe('GET /global-learning-xprize', function(){
     })
     it('should have a perks object', function(done){
         request(app)
-            .get('/global-learning-xprize')
+            .get('/kickstarter/332322466/gram-parsons-cosmic-revival-derry-down')
             .set('Accept', 'application/json')
             .end(function (err, res) {
                 expect(res.body).to.have.a.property("perks")
