c986e99e22aaf907674a2c8d5005e10257812109
Jake LeBoeuf <dev@jakeleboeuf.com>
2014-10-17 13:14:18 -0400

    allowing crossdomain requests

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Handle Cross domain requests
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// Handle Routes
 app.get('/', routes);
 app.get('/indiegogo/:id', indiegogo);
 app.get('/kickstarter/:userId/:projectName', kickstarter);
